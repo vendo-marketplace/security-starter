5b78d1005ec8fed69a6998f9020ba282ef231049
HaaaamBurger <gemasclashes@gmail.com>
2026-06-10 14:49:41 +0300

    audience bug fix

--- src/main/java/com/vendo/security_starter/jwt/JwtPayload.java
+++ src/main/java/com/vendo/security_starter/jwt/JwtPayload.java
@@ -1,10 +1,12 @@
 package com.vendo.security_starter.jwt;
 
 import java.util.Map;
+import java.util.Set;
 
 public record JwtPayload(
         String subject,
         Map<String, Object> claims,
+        Set<String> audience,
         long expiration) {
 
     public static Builder builder() {
@@ -14,6 +16,7 @@ public record JwtPayload(
     public static class Builder {
         private String subject;
         private Map<String, Object> claims;
+        private Set<String> audience;
         private long expiration;
 
         public Builder subject(String subject) {
@@ -31,8 +34,13 @@ public record JwtPayload(
             return this;
         }
 
+        public Builder audience(Set<String> audience) {
+            this.audience = audience;
+            return this;
+        }
+
         public JwtPayload build() {
-            return new JwtPayload(subject, claims, expiration);
+            return new JwtPayload(subject, claims, audience, expiration);
         }
     }
 
--- src/main/java/com/vendo/security_starter/jwt/JwtService.java
+++ src/main/java/com/vendo/security_starter/jwt/JwtService.java
@@ -3,6 +3,7 @@ package com.vendo.security_starter.jwt;
 import com.vendo.core_lib.utils.StringUtils;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.util.CollectionUtils;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
@@ -20,9 +21,14 @@ public class JwtService {
             throw new IllegalArgumentException("Invalid payload.");
         }
 
-        return Jwts.builder()
-                .subject(payload.subject())
+        JwtBuilder jwtBuilder = Jwts.builder();
+        if (!CollectionUtils.isEmpty(payload.audience())) {
+            payload.audience().forEach(aud -> jwtBuilder.audience().add(aud));
+        }
+
+        return jwtBuilder
                 .claims(payload.claims())
+                .subject(payload.subject())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + payload.expiration()))
                 .signWith(getSignInKey(secret), SignatureAlgorithm.HS256)
