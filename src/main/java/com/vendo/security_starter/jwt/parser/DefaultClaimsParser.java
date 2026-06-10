275a8eebdac5c0600f74853be77c95801fbc0fda
HaaaamBurger <gemasclashes@gmail.com>
2026-06-10 13:50:10 +0300

    improved roles logic

--- src/main/java/com/vendo/security_starter/jwt/parser/DefaultClaimsParser.java
+++ src/main/java/com/vendo/security_starter/jwt/parser/DefaultClaimsParser.java
@@ -1,14 +1,17 @@
 package com.vendo.security_starter.jwt.parser;
 
+import com.vendo.core_lib.utils.StringUtils;
 import com.vendo.security_lib.type.TokenClaim;
 import com.vendo.security_starter.jwt.JwtService;
 import io.jsonwebtoken.Claims;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import static com.vendo.core_lib.constants.Delimiters.COMMA_DELIMITER;
 
 public class DefaultClaimsParser implements TokenClaimsParser {
 
@@ -18,7 +21,7 @@ public class DefaultClaimsParser implements TokenClaimsParser {
     public TokenClaims extract(String token, String key) {
         try {
             Claims claims = JwtService.extractAll(token, key);
-            Set<String> roles = extractRoles(claims, TokenClaim.ROLES.getClaim());
+            Set<String> roles = extractRoles(claims);
             return new TokenClaims(claims.getSubject(), roles, claims.getAudience());
         } catch (Exception e) {
             log.error(e.getMessage());
@@ -26,20 +29,13 @@ public class DefaultClaimsParser implements TokenClaimsParser {
         }
     }
 
-    private Set<String> extractRoles(Claims claims, String rolesClaim) {
-        Object rawRoles = claims.get(rolesClaim);
+    private Set<String> extractRoles(Claims claims) {
+        String roles = claims.get(TokenClaim.ROLES.getClaim(), String.class);
 
-        if (rawRoles instanceof List<?> list) {
-            if (list.isEmpty()) {
-                return Set.of();
-            } else if (list.stream().allMatch(String.class::isInstance)) {
-
-                return list.stream()
-                        .map(String.class::cast)
-                        .collect(Collectors.toSet());
-            }
+        if (StringUtils.isEmpty(roles)) {
+            return Set.of();
         }
 
-        throw new IllegalStateException("Invalid roles claim.");
+        return Arrays.stream(roles.split(COMMA_DELIMITER)).collect(Collectors.toSet());
     }
 }
